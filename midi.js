0a285ac3201ac59d65770b4f44be84fea9147d04
Jentagawa <gitgawa@gmail.com>
2015-06-04 00:57:05 +0900

    sdfvgadgdas

--- midi.js
+++ midi.js
@@ -1,209 +1,44 @@
-// 2014.3.01 Chrome のWeb MIDI APIを使うために変更
-// 2013.9.14 バルクダンプを受け取るために、sysexをONにした
+log('MIDI controller detector started.');
 
-{
-	var m=null;
-	var inputs=null;
-	var input=null;
-	var outputs=null;
-	var output=null;
-	var input_device=0;
-	var output_device=0;
-	var log=null;
+function midiMessageReceived( e ) {
+  var cmd = e.data[0] >> 4;
+  var channel = e.data[0] & 0xf;
+  var b = e.data[1];
+  var c = e.data[2];
 
-	var time_interval=0;
-	var timerId=null;
-	var input_menu_id=null;
-	var output_menu_id=null;
-	var notes=[0x28, 0x2a, 0x2c, 0x2d, 0x2f, 0x31, 0x33, 0x34];
+    var obj = { 
+      
+        channel: b,
+        
+    };
+    $('#events').val($('#events').val() + '\r\n' + JSON.stringify(obj));
+}
 
-	var note_on_num=-1;	//Just sound note number
+//window.addEventListener('load', function() {
+  log('Connecting to controller.');
+  navigator.requestMIDIAccess().then( gotMIDI, didntGetMIDI );
+//});
 
-	function runTest()
-	{
-		if (!log)
-			log = document.getElementById("log");
-		navigator.requestMIDIAccess().then( success, failure );
-	}
+function gotMIDI( midiAccess ) {
+  midi = midiAccess;
+  if ((typeof(midiAccess.inputs) == "function")) {  //Old Skool MIDI inputs() code
+    var ins = midiAccess.inputs();
+    log('All inputs: ', JSON.stringify(ins));
+    for (var i=0; i<ins.length; i++)
+      ins[i].onmidimessage = midiMessageReceived;
+  } else {
+    var inputs=midiAccess.inputs.values();
+    for ( var input = inputs.next(); input && !input.done; input = inputs.next())
+      input.value.onmidimessage = midiMessageReceived;
+  }
+}
 
-	function runTest2()
-	{
-		if (!log)
-			log = document.getElementById("log");
-		navigator.requestMIDIAccess( { sysex: true } ).then( success, failure );
-	}
-
-	function success(midiAccess)
-	{
-		m=midiAccess;
-
-		if (typeof m.inputs === "function") {
-			inputs=m.inputs();
-			outputs=m.outputs();
-		} else {
-			var inputIterator = m.inputs.values();
-			inputs = [];
-			for (var o = inputIterator.next(); !o.done; o = inputIterator.next()) {
-				inputs.push(o.value)
-			}
-
-			var outputIterator = m.outputs.values();
-			outputs = [];
-			for (var o = outputIterator.next(); !o.done; o = outputIterator.next()) {
-				outputs.push(o.value)
-			}
-		}
-
-		alert( "OK MIDI が使えます" );
-		if(input_menu_id!=null) setInputDeviceSelect();
-		if(output_menu_id!=null) setOutputDeviceSelect();
-	}
-
-	function failure(error)
-	{
-		alert( "NG MIDI が使えません！" )
-	}
-
-	function goSound(value){
-		if(outputs==null) {
-			alert( "NG MIDI が使えません！");
- 			clearInterval(timerId);
-			return;
-		} else {
-			if(output==null) output=outputs[output_device];
-//			output=m.getOutput(0);
-			output.send( [0x90, value, 0x7f] );
-			output.send( [0x90, value, 0x00],window.performance.now()+1000.0);
-		}
-	}
-
-	function soundChange(parts){
-		var value=0;
-		if(outputs==null) alert( "NG MIDI が使えません！" );
-		if(output==null) output=outputs[output_device];
-		value = parts.options[parts.selectedIndex].value;
-		output.send( [0xC0, value] );
-	}
-
-	function continueSound(){
-		continueSoundSub();
-	}
-
-	function continueSoundSub(){
-		var i=0;
-		timerId=setInterval(function(){
-			goSound(notes[i]);
-		i++;
-		if(i>=notes.length) { i=0; }
-		}, 500);
-	}
-
-	function outMessage(data1,data2,data3)
-	{
-		if(output!=null) output.send([data1,data2,data3], 0);
-	}
-
-	function stopSound(){
-		output.send([0xb0,  0x78, 0x00], 0);
-		clearInterval(timerId);
-	}
+function didntGetMIDI( error ) {
+  log("No MIDI access: " + error.code );
+}
 
 
-
-	function setOutputMenuID(parts)
-	{
-		output_menu_id = parts;
-	}
-
-	function setOutputDeviceSelect(){
-		var i=0;
-		if(m!=null){
-			//--- 使用可能なデバイスの名前をメニューに設定する ---
-			if(outputs.length>0){
-				for(i=0; i<outputs.length; i++)
-					output_menu_id.options[i+1] = new Option(outputs[i].name, i+1);
-			}
-			output_menu_id.value=1;
-			output_device=0;
-			output=outputs[output_device];
-
-		}
-
-	}
-
-
-	function setInputMenuID(parts)
-	{
-		input_menu_id = parts;
-	}
-
-	function setInputDeviceSelect(){
-		var i=0;
-		if(m!=null){
-			//--- 使用可能なデバイスの名前をメニューに設定する ---
-			if(inputs.length>0){
-				for(i=0; i<inputs.length; i++)
-					input_menu_id.options[i+1] = new Option(inputs[i].name, i+1);
-			}
-		} 
-	}
-
-	function inputDeviceSelect(item){
-		input_device = item.options[item.selectedIndex].value-1;
-		if(input_device==-1) input_device=0;
-
-		log.innerText +="input_device=";
-		log.innerText += input_device;
-		log.innerText +="\n";
-
-		input= inputs[input_device];			//add for Page 18, MIDI message monitor
-		input.onmidimessage = handleMIDIMessage1; 	//add for Page 18, MIDI message monitor
-
-//		input._inLongSysexMessage=true;
-//		input.addEventListener( "midimessage", handleMIDIMessage1 );
-	}
-
-	function outputDeviceSelect(item){
-		output_device = item.options[item.selectedIndex].value-1;
-		if(output_device==-1) output_device=0;
-		output=outputs[output_device];
-		log.innerText +="output_device=";
-		log.innerText += output_device;
-		log.innerText +="\n";
-	}
-
-//add for Page 18, MIDI message monitor
-	function handleMIDIMessage1( event ) {
-	var str=null;
-	var i,k;
-
-	if( event.data[0] ==0xFE ) return;
-
-	if( event.data.length>1) {
-		str ="data.length=" +event.data.length+ ":"+ " 0x" + event.data[0].toString(16) + ":";
-		log.innerText += str;
-
-		for(i=1,k=0; i<event.data.length; i++, k++){
-				str =" 0x" + event.data[i].toString(16) + ":";
-				log.innerText += str;
-				if(i%8==0){
-					log.innerText +="\n";
-				}
-			}
-		}
-		str ="\n"; log.innerText += str;
-	}
-
-	function goSysextest()
-	{
-		var sysex=[0xF0, 0x43, 0x40, 0x03, 0x03, 0xF7];
-//		var sysex2=[0x03, 0xF7];
-//		var sysex=[0xF0, 0x43, 0xF7];
-        var now=window.performance.now();
-		output.send(sysex,now+100);
-//        now=window.performance.now();
-//		output.send(sysex2,now+100);
-
-	}
-//--- End
+function log(text) {
+    $('#log').append(text +  '<br />');
+    
 }
--- script.js
+++ script.js
@@ -1,44 +1,176 @@
-og('MIDI controller detector started.');
+party = false;
 
-function midiMessageReceived( e ) {
-  var cmd = e.data[0] >> 4;
-  var channel = e.data[0] & 0xf;
-  var b = e.data[1];
-  var c = e.data[2];
+current_decks = {left: '0ymI54DuMj8',right:'jRetF46d2Vk'}
 
-    var obj = { 
-      
-        channel: b,
-        
-    };
-    $('#events').val($('#events').val() + '\r\n' + JSON.stringify(obj));
-}
-
-//window.addEventListener('load', function() {
-  log('Connecting to controller.');
-  navigator.requestMIDIAccess().then( gotMIDI, didntGetMIDI );
-//});
-
-function gotMIDI( midiAccess ) {
-  midi = midiAccess;
-  if ((typeof(midiAccess.inputs) == "function")) {  //Old Skool MIDI inputs() code
-    var ins = midiAccess.inputs();
-    log('All inputs: ', JSON.stringify(ins));
-    for (var i=0; i<ins.length; i++)
-      ins[i].onmidimessage = midiMessageReceived;
-  } else {
-    var inputs=midiAccess.inputs.values();
-    for ( var input = inputs.next(); input && !input.done; input = inputs.next())
-      input.value.onmidimessage = midiMessageReceived;
-  }
-}
-
-function didntGetMIDI( error ) {
-  log("No MIDI access: " + error.code );
-}
-
-
-function log(text) {
-    $('#log').append(text +  '<br />');
+function onYouTubePlayerReady(playerId) {
+    left_player = document.getElementById('left-player')
+    right_player = document.getElementById('right-player')
     
+    left_player.setVolume(100);
+    right_player.setVolume(0);
 }
+
+function load_vid(channel, video_id) {
+    var player_elem = document.getElementById(channel + '-player');
+    player_elem.loadVideoById(video_id);
+    current_decks[channel] = video_id;
+
+    document.location.hash = current_decks.left + '/' + current_decks.right;
+}
+
+$(function() {
+    $('#crossfader').slider({max: 200, min: 0,value: 0});
+    $('#crossfader').bind('slide', function(event, ui) {
+        var left_val = Math.max(Math.min(190 - parseInt($('#crossfader').slider('option','value')), 100), 0);
+        var right_val = Math.max(Math.min(parseInt($('#crossfader').slider('option','value')), 110) - 10, 0);
+
+        left_player.setVolume(left_val);
+        right_player.setVolume(right_val);
+        
+    });
+
+    function search_loaded() {
+        return true;
+    }
+    
+    google.load('search','1', {'callback': search_loaded});
+
+    function setup_goog() {
+        left_searcher = new google.search.VideoSearch();
+        left_searcher.setResultSetSize(google.search.Search.LARGE_RESULTSET);
+        
+        left_searcher.setSearchCompleteCallback(null, function() {
+            $('#left-crate').empty();            
+            for(var i = 0; i<left_searcher.results.length; i++) {
+                var result = left_searcher.results[i];
+                if(result.videoType != "YouTube") {
+                    continue;
+                }
+
+                var thumb_url = result.tbUrl;
+                var video_id = result.tbUrl.split('/')[4];
+                $('#left-crate').append(
+                    '<a class="crate-vid" href="javascript:void(0);" title="' + result.title + '" onclick="load_vid(\'left\',\'' + video_id + '\');"><img width="50" src="' + thumb_url + '" /></a>'
+                );
+            }
+            $('#left-crate .crate-vid').hover(function() {
+                $('#left-crate-title').text($(this).attr('title'));
+            }, function() {
+                $('#left-crate-title').empty();
+            });
+
+            if (left_searcher.cursor) {
+                $('#left-crate').append('<div class="pages"></div>');
+                var pages = left_searcher.cursor.pages;
+                for(var i=0; i<pages.length; i++) {
+                    if(i == left_searcher.cursor.currentPageIndex) {
+                        $('#left-crate .pages').append(
+                            '<span class="page">' + pages[i].label + '</a>'
+                        );
+                    } else {
+                        $('#left-crate .pages').append(
+                            '<a class="page" pagenum="' + i + '">' + pages[i].label + '</a>'
+                        );
+                    }
+                }
+                
+                $('#left-crate a.page').click(function() {
+                    left_searcher.gotoPage(parseInt($(this).attr('pagenum')));
+                });
+            }
+            
+        });
+
+        // right
+
+        right_searcher = new google.search.VideoSearch();
+        right_searcher.setResultSetSize(google.search.Search.LARGE_RESULTSET);
+        
+        right_searcher.setSearchCompleteCallback(null, function() {
+            $('#right-crate').empty();
+            
+            for(var i = 0; i<right_searcher.results.length; i++) {
+                var result = right_searcher.results[i];
+                if(result.videoType != "YouTube") {
+                    continue;
+                }
+                
+                var thumb_url = result.tbUrl;
+                var video_id = result.tbUrl.split('/')[4];
+                $('#right-crate').append(
+                    '<a class="crate-vid" href="javascript:void(0);" title="' + result.title + '" onclick="load_vid(\'right\',\'' + video_id + '\');"><img width="50" src="' + thumb_url + '" /></a>'
+                );
+            }
+            $('#right-crate .crate-vid').hover(function() {
+                $('#right-crate-title').text($(this).attr('title'));
+            }, function() {
+                $('#right-crate-title').empty();
+            });
+            
+            if (right_searcher.cursor) {
+                $('#right-crate').append('<div class="pages"></div>');
+                var pages = right_searcher.cursor.pages;
+                for(var i=0; i<pages.length; i++) {
+                    if(i == right_searcher.cursor.currentPageIndex) {
+                        $('#right-crate .pages').append(
+                            '<span class="page">' + pages[i].label + '</a>'
+                        );
+                    } else {
+                        $('#right-crate .pages').append(
+                            '<a class="page" pagenum="' + i + '">' + pages[i].label + '</a>'
+                        );
+                    }
+                }
+                
+                $('#right-crate a.page').click(function() {
+                    right_searcher.gotoPage(parseInt($(this).attr('pagenum')));
+                });
+            }
+            
+        });
+
+        // trigger the first search
+        left_searcher.execute($('#left-selector input').val());
+        right_searcher.execute($('#right-selector input').val());
+    }
+
+    google.setOnLoadCallback(setup_goog);
+    
+    $('#left-selector button').click(function() {
+        var input_val = $(this).siblings('input').val();
+        left_searcher.execute(input_val)
+    });
+
+    $('#left-selector input').keyup(function(ev) {
+        if(ev.keyCode == 13) {
+            $('#left-selector button').click();
+        }
+    });
+    
+    $('#right-selector button').click(function() {
+        var input_val = $(this).siblings('input').val();
+        right_searcher.execute(input_val)
+    });
+
+    $('#right-selector input').keyup(function(ev) {
+        if(ev.keyCode == 13) {
+            $('#right-selector button').click();
+        }
+    });
+
+    $('input[type=text]').focus(function() {
+      this.select();
+    });
+
+    $('#party').click(function() {
+      if(party) {
+        $('body').css('backgroundImage','url(purplestars.gif)');
+        $('#party').text('party');
+        party = false;
+      } else {
+        $('body').css('backgroundImage','url(party_animals_e0.gif)');
+        $('#party').text('serious');
+        party = true;
+      }
+    });
+});
